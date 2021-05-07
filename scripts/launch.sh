a526ac3848c7d9e4c574521cc9b66fd132926a84
Cristian Ramon-Cortes <cristian.ramon-cortes@dynatrace.com>
2021-05-07 17:08:03 +0200

    NOISSUE: Add launch and test scripts

---
+++ scripts/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+export PYTHONPATH="${PYTHONPATH}:${SCRIPT_DIR}/../src/"
+export PATH="${PATH}:${SCRIPT_DIR}/../chromedriver/"
+
+python "${SCRIPT_DIR}/../src/bots/telegram/reservation_bot.py"
